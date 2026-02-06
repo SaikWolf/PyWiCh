9c86f905046756aeca91e931cddb608aca52dec1
Bill Clark <saikou@rfml8>
2026-02-06 10:10:54 -0500

    Refactor into module should be installable now?

--- src/pywich/__init__.py
+++ src/pywich/__init__.py
@@ -1,3 +1,6 @@
 """This module has the main functions of the PYthon WIreless CHannel simulator."""
+
+__version__ = '1.01'
+
 from . import angles
 from . import scenarios,fading,antennas,channel_performance,frequency_band
